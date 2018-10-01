e4d99774fdb537a980dedd9a29ebc869506f5a35
Sheldrick <mrmayrant@yahoo.com>
2018-10-01 07:51:03 -0400

    added click and keypress listeners to execute ctrlAddItem function

--- budgety/app.js
+++ budgety/app.js
@@ -1,24 +1,25 @@
-var budgetController = (function() {
-  var x = 23;
-  var add = function(a) {
-    return x + a;
-  };
-
-  return {
-    publicTest: function(b) {
-      return add(b);
-    }
-  };
-})();
+var budgetController = (function() {})();
 
 var UIController = (function() {})();
 
 // module to connect the two above modules together
 var controller = (function(budgetCtlr, UICtlr) {
-  var z = budgetCtlr.publicTest(5);
-  return {
-    publicMethod: function() {
-      console.log(z);
-    }
+  var ctrlAddItem = function() {
+    // get input data
+    // add item to budget controller
+    // add new item to UI
+    // calculate budget
+    // display budget on UI
+    console.log("works");
   };
+
+  // the ctrlAddItem function is executed when the user clicks the add button
+  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+
+  // If the user presses the enter key, execute the ctrlAddItem function
+  document.addEventListener("keypress", function(event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  });
 })(budgetController, UIController);
