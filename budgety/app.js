88a88225bc61ee743f20dfb14810b82f6f2f9177
Sheldrick <mrmayrant@yahoo.com>
2018-10-02 16:53:57 -0400

    added the basic functioality to add a new item to the budget controller

--- budgety/app.js
+++ budgety/app.js
@@ -1,22 +1,61 @@
 var budgetController = (function() {
+  // expense function constructor
   var Expense = function(id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
   };
 
+  // income function constructor
   var Income = function(id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
   };
-})();
 
-var Expense = function(id, description, value) {
-  this.id = id;
-  this.description = description;
-  this.value = value;
-};
+  var allExpenses = [];
+
+  var allIncomes = [];
+
+  var data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  };
+
+  return {
+    addItem: function(type, des, val) {
+      var newItem, ID;
+      // create ID as long as ther is an item in the array
+      if (data.allItems[type].length > 0) {
+        // create ID
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      // a new expense or income object is created based on the type that the user utilizes
+      if (type === "exp") {
+        newItem = new Expense(ID, des, val);
+      } else if (type === "inc") {
+        newItem = new Income(ID, des, val);
+      }
+
+      // each item is stored in the array of the allItems object that's within the data object
+      data.allItems[type].push(newItem);
+
+      return newItem;
+    },
+    testing: function() {
+      console.log(data);
+    }
+  };
+})();
 
 var UIController = (function() {
   // object to store DOM strings
@@ -61,9 +100,12 @@ var controller = (function(budgetCtlr, UICtlr) {
   };
 
   var ctrlAddItem = function() {
+    var input, newItem;
+
     // get input data
-    var input = UIController.getInput();
+    input = UICtlr.getInput();
     // add item to budget controller
+    newItem = budgetCtlr.addItem(input.type, input.description, input.value);
     // add new item to UI
     // calculate budget
     // display budget on UI
@@ -78,4 +120,5 @@ var controller = (function(budgetCtlr, UICtlr) {
   };
 })(budgetController, UIController);
 
+// invoking the init method
 controller.init();
